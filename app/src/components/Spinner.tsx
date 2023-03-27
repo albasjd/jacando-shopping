0849ee86db5a15edcdeef0fdafa7fbacf6bfc051
Joao Dias <j.dias@planerio.de>
2023-03-27 12:00:47 +0200

    adds spinner

---
+++ app/src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-x-8 dark:border-gray-50 border-gray-800"></div>
+    </div>
+  );
+}
