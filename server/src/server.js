96aac126f6cc2d280260e6d5418bd2542c2b04f2
Joao Dias <j.dias@planerio.de>
2023-03-27 11:26:32 +0200

    setup express, middlewares, apolloServer and mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,48 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
+import http from 'http';
+import cors from 'cors';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+import typeDefs from './graphql/typeDefs.js';
+import resolvers from './graphql/resolvers.js';
 
 // Load environment variables
 dotenv.config();
 
 const PORT = process.env.SERVER_PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = process.env.MONGODB_NAME;
 
 // Set up Express
 const app = express();
+const httpServer = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('Jacando Server Hello World');
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running at http://localhost:${PORT}`)
+await server.start();
+
+// Set up Express middlewares
+app.use(cors(), bodyParser.json(), expressMiddleware(server));
+
+// Connect to MongoDB
+mongoose
+.connect(MONGODB_URI, {
+  dbName: DB_NAME,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  return httpServer.listen(PORT, () =>
+    console.log(`🚀 Server is running at http://localhost:${PORT}`)
+  );
 });
