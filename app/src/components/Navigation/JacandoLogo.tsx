ea4fda17735227c0549d7dd743996a25e79df36e
Joao Dias <j.dias@planerio.de>
2023-03-27 12:26:37 +0200

    adds JacandoLogo

---
+++ app/src/components/Navigation/JacandoLogo.tsx
@@ -0,0 +1,9 @@
+export default function JacandoLogo() {
+  return (
+    <img
+      src="https://www.jacando.com/wp-content/uploads/2021/01/jacando_logo_neg.png"
+      alt="Jacando Logo"
+      className="w-32"
+    />
+  );
+}
