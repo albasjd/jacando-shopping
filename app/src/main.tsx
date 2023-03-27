bceae1b58962f75ed58c124920b06ee8a09d8b96
Joao Dias <j.dias@planerio.de>
2023-03-27 11:41:45 +0200

    setup apollo client and browser router adds some base css

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-import App from './App'
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "./index.css";
+
+const uri = "http://localhost:3001/graphql";
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache,
+  uri,
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>
+);
